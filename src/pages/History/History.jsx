41113e5c651a105dacefb26b011bb9d73e426999
nicholaskim7 <nickdraggy@gmail.com>
2025-06-23 23:06:28 -0500

    render fetched workout sessions

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,13 +1,123 @@
 import React from 'react';
 import './History.css';
+import { useState, useEffect } from 'react';
 
 function History() {
+  const [fetchedWorkouts, setFetchedWorkouts] = useState([]);
+  const [error, setError] = useState(null); 
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api/fetch-workouts");
+        if (!response) {
+          throw new Error('Network response was not ok');
+        }
+        const jsonData = await response.json(); 
+        setFetchedWorkouts(jsonData);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchWorkouts();
+  }, []);
+
+
   return (
-    <div className='custom-container'>
-      <div>History</div>
-      <div>History</div>
-      <div>History</div>
-      <div>History</div>
+    // example format of fetched workout data
+
+    //     [
+    //   {
+    //     "workoutId": 2,
+    //     "date": "2025-06-23T22:32:53.000Z",
+    //     "exercises": [
+    //       {
+    //         "name": "Incline Dumbbell Bench Press",
+    //         "sets": [
+    //           {
+    //             "reps": 10,
+    //             "weight": 45
+    //           },
+    //           {
+    //             "reps": 10,
+    //             "weight": 45
+    //           },
+    //           {
+    //             "reps": 8,
+    //             "weight": 55
+    //           },
+    //           {
+    //             "reps": 5,
+    //             "weight": 55
+    //           }
+    //         ]
+    //       },
+    //       {
+    //         "name": "Tricep Pulldowns",
+    //         "sets": [
+    //           {
+    //             "reps": 10,
+    //             "weight": 25
+    //           },
+    //           {
+    //             "reps": 10,
+    //             "weight": 35
+    //           },
+    //           {
+    //             "reps": 10,
+    //             "weight": 55
+    //           }
+    //         ]
+    //       },
+    //       {
+    //         "name": "Shoulder Press",
+    //         "sets": [
+    //           {
+    //             "reps": 8,
+    //             "weight": 35
+    //           },
+    //           {
+    //             "reps": 8,
+    //             "weight": 35
+    //           },
+    //           {
+    //             "reps": 5,
+    //             "weight": 35
+    //           }
+    //         ]
+    //       }
+    //     ]
+    //   },
+    // ]
+
+    <div>
+      <h2>Workout History</h2>
+      <ul>
+        <div className='custom-container'>
+          {fetchedWorkouts.map((workout) => (
+            //iterate workout sessions
+            <li key={workout.workoutId}> 
+              <h4>{new Date(workout.date).toLocaleDateString()}</h4>
+              <ul>
+                <div className='exercises-container'>
+                  {workout.exercises.map((exercise, idx) => ( // each workout session has a list of exercises that were done
+                    <li key={idx}>
+                      <strong className='exercise-name'>{exercise.name}</strong>
+                      <ul className='list-of-sets'>
+                        {exercise.sets.map((set, setIdx) => ( // each exercise has its respective sets and reps
+                          <li key={setIdx}>
+                            Set {setIdx + 1}: Reps: {set.reps}, Weight: {set.weight} lbs
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                  ))}
+                </div>
+              </ul>
+            </li>
+          ))}
+        </div>
+      </ul>
     </div>
   )
 }
