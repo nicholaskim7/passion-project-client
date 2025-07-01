05a30b6e424bb4e3240b691c1e18ae49774a3f6b
nicholaskim7 <nickdraggy@gmail.com>
2025-06-30 20:35:46 -0500

    logout

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,6 +5,19 @@ import { FaBars } from 'react-icons/fa';
 import './Menu.css';
 
 function Menu() {
+  const handleLogout = async () => {
+    try {
+      await fetch('https://passion-project-server.onrender.com/api/logout', {
+        method: 'GET',
+        credentials: 'include'
+      });
+      window.location.href = '/login';
+    } catch (error) {
+      console.error('Logout failed:', error);
+    }
+  };
+
+
   return (
     <div className="menu-container">
       <Dropdown>
@@ -17,6 +30,7 @@ function Menu() {
           <Dropdown.Item href="/logworkout" className='item'>Log workout</Dropdown.Item>
           <Dropdown.Item href="/history" className='item'>Workout History</Dropdown.Item>
           <Dropdown.Item href="/your-prs" className='item'>Prs</Dropdown.Item>
+          <Dropdown.Item onClick={handleLogout} className='logout-button'>Logout</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     </div>
