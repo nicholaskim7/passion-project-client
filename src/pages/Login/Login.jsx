1b85a7d74648c9ac7b5582075725a0d1d3b7a182
nicholaskim7 <nickdraggy@gmail.com>
2025-07-01 11:30:37 -0500

    revert login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,41 +1,13 @@
-import React from 'react';
-import './Login.css';
+import React, { useEffect } from 'react';
 
 function Login() {
-  const handleLogin = () => {
-    const popup = window.open(
-      'https://passion-project-server.onrender.com/api/auth/google',
-      '_blank',
-      'width=500,height=600'
-    );
-
-    const checkPopup = setInterval(() => {
-      if (!popup || popup.closed) {
-        clearInterval(checkPopup);
-
-        // after popup closes, fetch session info
-        fetch('https://passion-project-server.onrender.com/api/auth/me', {
-          credentials: 'include',
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            if (data.user) {
-              console.log('User:', data.user);
-              window.location.href = '/';
-            } else {
-              console.log('Login failed or canceled.');
-            }
-          });
-      }
-    }, 500);
-  };
+  useEffect(() => {
+    window.location.href = 'https://passion-project-server.onrender.com/api/auth/google';
+  }, []);
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-      {/* dont open popup immediately only by user interaction */}
-      <button className="button" onClick={handleLogin} >
-        Log In with Google
-      </button>
+    <div>
+      <p>Redirecting to Google login...</p>
     </div>
   );
 }
