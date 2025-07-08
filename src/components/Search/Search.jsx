3fa4902d4d0e4a095efca1c500b1eb1fc4ed250a
nicholaskim7 <nickdraggy@gmail.com>
2025-07-07 19:35:30 -0500

    add debouncedSearch delay to trigger search only once user is done typing

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Search({ onSearch }) {
   const [searchUser, setSearchUser] = useState('');
+  const [debouncedSearch, setDebouncedSearch] = useState('');
+
+  useEffect(() => {
+    // add a delay to only trigger search once user is done typing
+    const handler = setTimeout(() => {
+      setDebouncedSearch(searchUser);
+    }, 300);
+
+    return () => clearTimeout(handler);
+  }, [searchUser])
+
+  // only call onSearch when debouncedSearch updates
+  useEffect(() => {
+    if (debouncedSearch.trim() !== '') {
+
+      // pass the updated search to parent component
+      onSearch(debouncedSearch);
+    }
+  }, [debouncedSearch]);
 
   const handleChange = (event) => {
     setSearchUser(event.target.value);
-    
-    // pass the updated search to parent component
-    onSearch(event.target.value);
   };
 
   return (
