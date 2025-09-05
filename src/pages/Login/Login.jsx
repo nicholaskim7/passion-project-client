a98e597c3186963b071f0dd283bac985339806b7
nicholaskim7 <nickdraggy@gmail.com>
2025-09-05 15:47:48 -0500

    update login page to allow user to click to be redirected to google auth

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,22 @@
 import React, { useEffect } from 'react';
+import './Login.css';
 
 function Login() {
-  useEffect(() => {
+  const handleGoogleLogin = () => {
     window.location.href = 'https://passion-project-server.onrender.com/api/auth/google';
-  }, []);
+  };
 
   return (
-    <div>
-      <p>Redirecting to Google login...</p>
+    <div className='login-container'>
+      <h1>Welcome to <span className='text'>SeungFit</span></h1>
+      <p>Please sign in to continue.</p>
+      <button className='google-btn' onClick={handleGoogleLogin}>
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg" 
+          alt="Google logo" 
+        />
+        Sign in with Google
+      </button>
     </div>
   );
 }
