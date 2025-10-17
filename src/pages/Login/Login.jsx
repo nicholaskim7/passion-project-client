037a69b8a45f2459b641e3e71a7936460ea83b61
nicholaskim7 <nickdraggy@gmail.com>
2025-10-17 16:40:20 -0500

    include credentials in Login.jsx so that cross-site cookie can be shared and not discarded

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -18,6 +18,7 @@ function Login() {
         headers: {
           'Content-Type': 'application/json',
         },
+        credentials: 'include',
         body: JSON.stringify({ token }),
       });
 
